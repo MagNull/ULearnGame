7a1a5ab645bb872d5514c22cc714706fc01afa8a
MagNull <pathfo@yandex.ru>
2022-06-04 13:30:57 +0500

    Change menu and pause

---
+++ Assets/Sources/Runtime/UI & HUD/Menu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Sources.Runtime.UI___HUD
+{
+    public class Menu : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _playButton;
+        [SerializeField]
+        private Button _exitButton;
+
+        private void Start()
+        {
+            var sceneLoader = FindObjectOfType<SceneLoader>();
+            _playButton.onClick.AddListener(() =>
+            {
+                Time.timeScale = 1;
+                sceneLoader.LoadHub();
+            });
+
+            _exitButton.onClick.AddListener(Application.Quit);
+        }
+    }
+}
